61ac44da02752f2bf0662a5bea75ef466321adee
Igor Altino <igor.altino@hotmail.com>
2022-12-30 10:38:46 -0300

    teste

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//1. Crie uma variavel para armazenar o nome
+//do carro e ponha o valor Ferrari. E exiba-a
+
+let nome = "Ferrari";
+console.log(nome);
+
+//2. Crie uma variavel para armazenar o preço
+//do bolo, que custa R$ 19,99.E exiba-a
+
+let preco = 19.99;
+console.log(preco);
+
+//3. Corrija o código a seguir (e exiba-o).
+// let "cidade" = São Paulo
+
+let cidade = "São Paulo";
+console.log(cidade);
